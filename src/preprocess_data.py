f32d6668fe46a13448dd73e3d4d4e044b4100b87
Karthik <kvasu2@illinois.edu>
2024-07-14 14:55:31 -0500

    Added script to create labeled data

--- src/preprocess_data.py
+++ src/preprocess_data.py
@@ -1,4 +1,8 @@
 import zipfile
+import os
+import pandas as pd
+
+
 
 def main():
     zip_file = "data/sample.zip"
@@ -6,6 +10,37 @@ def main():
 
     with zipfile.ZipFile(zip_file, 'r') as zip_ref:
         zip_ref.extractall(output_dir)
+    
+    parent_dir = os.path.abspath(os.path.join(os.getcwd(), os.pardir))
+
+    metadata = pd.read_csv(os.path.join(parent_dir,"data" , 'sample_labels.csv'))
+
+    conditions = [
+    "Hernia",
+    "Pneumonia",
+    "Fibrosis",
+    "Edema",
+    "Emphysema",
+    "Cardiomegaly",
+    "Pleural_Thickening",
+    "Consolidation",
+    "Pneumothorax",
+    "Mass",
+    "Nodule",
+    "Atelectasis",
+    "Effusion",
+    "Infiltration"
+    ]
+
+    for condition in conditions:
+        for condition in conditions:
+            metadata.loc[metadata['Finding Labels'].str.contains(condition), condition] = 1
+            metadata.loc[~metadata['Finding Labels'].str.contains(condition), condition] = 0
+
+
+    labels = metadata[["Image Index","Emphysema"]]
+    labels.to_csv(os.path.join(parent_dir, 'data', 'sample', 'labels.csv'), index=False)
+
 
 if __name__ == "__main__":
     main()
